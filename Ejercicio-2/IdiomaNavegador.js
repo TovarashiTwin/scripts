74a29d6e1672a8dbd5d34bbc6bed71f6e218d162
Manuel Lorenzo Vega <UO265009@uniovi.es>
2021-11-08 14:52:00 +0100

    ejercicio 2

---
+++ Ejercicio-2/IdiomaNavegador.js
@@ -0,0 +1,6 @@
+document.write("<p>Idioma: ")
+document.write(infoNavegador.idioma);
+document.write("</p>")
+document.write("<p>Todos los idiomas del usuario: ")
+document.write(infoNavegador.idiomas);
+document.write("</p>")
