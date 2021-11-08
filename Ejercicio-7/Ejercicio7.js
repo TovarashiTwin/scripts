90591e1dde6320103c5835277200dffc642ee0bd
Manuel Lorenzo Vega <UO265009@uniovi.es>
2021-11-08 14:25:33 +0100

    ejercicio 7 orientado a objetos

--- Ejercicio-7/Ejercicio7.js
+++ Ejercicio-7/Ejercicio7.js
@@ -1,39 +1,57 @@
-function ejercicio7() {
-	$("#ocultar_mostrar").click(function(){
-		if($("#fruta").is(":visible")){
-			$("#fruta").hide();
+class Ejercicio7 {
+
+	ocultar(){//Tarea 1
+		if($("[id=fruta]").is(":visible")){
+			$("[id=fruta]").hide();
 		}
 		else {
-			$("#fruta").show();
+			$("[id=fruta]").show();
 		}
-	});
-	$("#add").click(function(){
-		$("#ocultar_mostrar").after("<p id=\"fruta\"> " + $("#inputParrafo").val() + "</p>");
-	});
-	$("#delete").click(function(){
-		$("#fruta").first().remove();
-	});
-	$("#elementoPadre").click(function(){
+		
+	}
+
+	modificar(){//tarea 2
+		$("[id=fruta]").text(function(i, origText){
+			return origText.toUpperCase(); 
+		  });
+	}
+
+	add(){	//tarea 3
+		$("#modificar").after("<p id=\"fruta\"> " + $("#inputParrafo").val() + "</p>");
+		
+	}
+	delete(){//tarea 4
+		$("#fruta").remove();
+		
+	}
+	mostrarElementoPadre(){		//tarea 5
 		$("*", document.body).each(function() {
 			var etiquetaPadre = $(this).parent().get(0).tagName;
 			$(this).prepend(document.createTextNode( "Padre : <" + etiquetaPadre + "> elemento : <" + $(this).get(0).tagName +"> valor:"));
 		});
-	});
-	$("#sumTabla").click(function(){
+		
+	}
+	sumTabla(){ //tarea 6
+		console.log("Tarea 6")
 		var sumRows = 0;
 		var sumColumns = 0;
-		$("table").children().children().each(function() {
+		$("table").children().children().each(function() {			
 			sumRows++;
 		});
 		$("table").children().children().last().children().each(function() {
 			sumColumns++;
 		});
 		$("#sumTabla").after("<p>Sumatorio Filas: " + sumRows + " Sumatorio Columnas: " + sumColumns +  "</p>");		
-	});
+		
+	}
+	
+	
 }
 
-function init(){
-	ej = new ejercicio7();
-}
+// function init(){
+// 	ej = new ejercicio7();
+// }
 	
-onload = init
+// onload = init
+
+var ej = new Ejercicio7();
