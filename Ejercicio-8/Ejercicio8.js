c615260a3a59b65dac564179776c1942c85f4920
TovarashiTwin <tecmanuellor1516@gmail.com>
2021-11-09 13:39:07 +0100

    ejercicio 8 sin validar

--- Ejercicio-8/Ejercicio8.js
+++ Ejercicio-8/Ejercicio8.js
@@ -4,61 +4,23 @@ class Tiempo{
         this.unidades = "&units=metric";
         this.idioma = "&lang=es";
         this.http_request = false;
-        this.lugares = "Madrid";//["Madrid","Oviedo","Ávila","Badalona","Trinidad"];
+        this.lugares = ["Madrid","Oviedo","Ávila","Badalona","Trinidad"];
     }
     
-    //Esto es para leer un xml
-    makeRequest () { 
-        if (window.XMLHttpRequest) { //Navegadores modernos          
-            this.http_request = new XMLHttpRequest();
-            if (this.http_request.overrideMimeType) {
-                this.http_request.overrideMimeType('text/xml');
-            }
-        } else if (window.ActiveXObject) { //Navegadores antiguos
-            this.http_request = new ActiveXObject("Microsoft.XMLHTTP");
-        } 
-
-        if (!this.http_request) {
-            alert('Ha ocurrido un error inesperadao');
-                return false;
-        }
-        if (this.http_request.readyState !== 4) {
-            console.log("Un saludo");
-        }
-        var self = this;
-        this.http_request.onreadystatechange = function(){            
-            if (self.http_request.readyState == 4) {//request finished and response is ready 
-                if (self.http_request.status == 200) {//"OK"
-                    var xmldoc = self.http_request.responseXML;
-                    var root_node = xmldoc.getElementsByTagName('ruta').item(0);
-                    var hitos = root_node.getElementsByTagName('hito');
-                    for (let i = 0; i < hitos.length; i++){
-                        let hito = hitos.item(i);
-                        let coordenadas = hito.getElementsByTagName('coordenadas').item(0);
-                        let latitud = coordenadas.getElementsByTagName('latitud').item(0);
-                        let longitud = coordenadas.getElementsByTagName('longitud').item(0);
-                        self.cargarDatos(hito);
-                    }
-                } else {
-                    alert('Ha ocurrido un error con la petición');
-                }
-            }           
-        };
-        
-        this.http_request.open('GET', this.url, true);
-        this.http_request.send(null);
+    
+    cargarTodosLugares(){
+        for (let i = 0; i < this.lugares.length; i++) {
+            this.cargarDatos(this.lugares[i]);
+        }        
     }
 
     //Esto para cargarlo, como tenemos ya las ciudades es mas sencllo
-    cargarDatos(lugar){
-        
-        let coordenadas = lugar.getElementsByTagName('coordenadas').item(0);
-        let latitud = coordenadas.getElementsByTagName('latitud').item(0).getAttribute('grados');
-        let longitud = coordenadas.getElementsByTagName('longitud').item(0).getAttribute('grados');
-        var self = this;
+    cargarDatos(lugar){                
+        var self = this;//Esto es necesario por como funciona this en js
+
         $.ajax({
             dataType: "json",
-            url: "http://api.openweathermap.org/data/2.5/weather?lat=" + latitud + "&lon=" + longitud + this.unidades + this.idioma + "&APPID=" + this.apikey,
+            url: "http://api.openweathermap.org/data/2.5/weather?q="+lugar+ this.unidades + this.idioma + "&APPID=" + this.apikey,
             method: 'GET',
             success: function(data){
                 self.datos = data;                
@@ -72,7 +34,7 @@ class Tiempo{
     verDatos(lugar){
         let lugarLista = $(document.createElement("li"));
         let titulo = $(document.createElement("h2"));
-        let nombre = document.createTextNode(lugar.getAttribute("nombre"));
+        let nombre = document.createTextNode(lugar);
         titulo.append(nombre);
         lugarLista.append(titulo);
         
@@ -120,6 +82,6 @@ class Tiempo{
 }
 function init(){
     var elTiempo = new Tiempo();
-    elTiempo.makeRequest();
+    elTiempo.cargarTodosLugares();
 }
 onload = init;
