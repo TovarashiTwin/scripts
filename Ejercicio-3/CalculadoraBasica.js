9b9e2dfc2a96123387173f0ed2ababb014704e70
TovarashiTwin <tecmanuellor1516@gmail.com>
2021-11-09 14:50:49 +0100

    calculadora basica funcional, falta memoria y validacion

--- Ejercicio-3/CalculadoraBasica.js
+++ Ejercicio-3/CalculadoraBasica.js
@@ -3,22 +3,80 @@ class CalculadoraBasica{
 		this.pantalla="";
         this.memoria=0.0;
         this.auxMemory = false;
+
+        this.primerNumero = null;//Esto lo meteremos en un Number
+        this.operacion = "";
+        
+        //aqui vamos metiendo los numeros en formato string hasta que llegue la hora de convertirlo
+        this.numeroProvisional = "";
+
 	}
     display(){
         //console.log(this.pantalla);
         document.getElementById('pantalla').value = this.pantalla;//TODO se puede usar getElementById?
     }
-    botonSimple(arg){
-        //Esta funcion se usa para los botones que simplemente añaden su simbolo
+
+
+    botonNumerico(arg){
+        //Esta funcion se usa para los numeros del 0 al 9
         this.auxMemory = false;
+
+        this.numeroProvisional += arg; //vamos almacenando los numeros como string, luego lo pasaremos a number
         this.pantalla+=arg;
+        this.display();
+    }
+
+    botonOperacion(arg){
+        //esta funcion se usa para + - / *
+        this.operacion = arg;//Si pulsamos otra se sobreescribe
+        //persistimos el primer numero y borramos la pantalla
+        this.primerNumero = new Number(this.numeroProvisional);
+        this.numeroProvisional = "";
+        this.pantalla = "";
         this.display();        
     }
+
+
     botonClear(){
         this.auxMemory = false;
         this.pantalla ="";
+        this.numeroProvisional = "";
+        this.primerNumero = null;
         this.display();        
     }
+
+
+    calcularResultado(){
+        this.auxMemory = false;
+        try{
+            //Dependiendo de la operacion necesitemos uno o dos numeros
+            if(this.primerNumero != null){
+                let segundoNumero = new Number(this.numeroProvisional);
+                //let toEval = this.primerNumero + this.operacion +segundoNumero;
+                //console.log(toEval);                
+                let resultado = eval(this.primerNumero + this.operacion +segundoNumero);
+
+                //Lo mostramops
+                document.getElementById('pantalla').value = resultado;
+                this.pantalla = resultado;
+                this.primerNumero = null;//document.getElementById('pantalla').value;
+                this.numeroProvisional = resultado;
+            }else{
+                console.log("Faltaba primer numero");
+                //document.getElementById('pantalla').value = "Syntax Error";
+            }
+            
+            //document.getElementById('pantalla').value = eval(this.pantalla);
+            
+        }catch(excepcion){
+            console.log(excepcion);
+            document.getElementById('pantalla').value = "Syntax Error";
+            //console.log(this.memoria);
+        }
+    }  
+
+
+    ///BOTONES ESPECIALES DE MEMORIA
     botonMemSum(){
         this.auxMemory = false;
         this.calcularResultado();
@@ -59,16 +117,7 @@ class CalculadoraBasica{
         }
 
     }
-    calcularResultado(){
-        this.auxMemory = false;
-        try{            
-            document.getElementById('pantalla').value = eval(this.pantalla);
-            this.pantalla = document.getElementById('pantalla').value;
-        }catch(excepcion){
-            document.getElementById('pantalla').value = "Syntax Error";
-            //console.log(this.memoria);
-        }
-    }  
+    
     
 }
 var calculadora = new CalculadoraBasica();
