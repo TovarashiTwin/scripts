6bafe253d554cba0c575c7bcab8c1cd49551b675
TovarashiTwin <tecmanuellor1516@gmail.com>
2021-11-13 21:22:01 +0100

    layout completo cientifica y funciones trigonometricas

--- Ejercicio-4/CalculadoraCientifica.js
+++ Ejercicio-4/CalculadoraCientifica.js
@@ -4,11 +4,30 @@ class CalculadoraBasica{
 		this.pantalla="";
         this.memoria=0.0;
         this.auxMemory = false;
+        this.inputkey();
+        
 	}
     display(){
         //console.log(this.pantalla);
         document.getElementById('pantalla').value = this.pantalla;//TODO se puede usar getElementById?
+        if(this.pantalla == "Syntax Error"){
+            console.log("Syntax Error")
+            this.pantalla == "";
+        }
     }
+    inputkey() {
+        document.addEventListener("keydown", (event) => {
+            if (["*", "/", "-", "+", "."].some(e => event.key.includes(e))) {
+                this.botonSimple(event.key);
+            }
+            else if (!isNaN(event.key)) {//comprobamos que es un numero
+                this.botonSimple(event.key);
+            }
+            else if (event.key == "Enter") {
+                this.calcularResultado();
+            }
+        })
+    };
     botonSimple(arg){
         //Esta funcion se usa para los botones que simplemente añaden su simbolo
         this.auxMemory = false;
@@ -60,9 +79,15 @@ class CalculadoraBasica{
         }
 
     }
+
+    botonEnter(){
+        this.calcularResultado();
+        this.display();
+
+    }
     calcularResultado(){
         this.auxMemory = false;
-        try{
+        try{            
             let pantallaAsString = this.pantalla;
             let toEval = "";
             let aux = "";
@@ -85,10 +110,12 @@ class CalculadoraBasica{
                 toEval += "new Number(" + aux +")"; 
             }
             console.log(toEval) ;
-            document.getElementById('pantalla').value = eval(toEval);
-            this.pantalla = document.getElementById('pantalla').value;
+            
+            this.pantalla =  eval(toEval).toString();//El toString es para que siempre se interprete como string y funcione todo bien
+            // document.getElementById('pantalla').value = this.pantalla;
         }catch(excepcion){
-            document.getElementById('pantalla').value = "Syntax Error";
+            this.pantalla = "Syntax Error";
+            //document.getElementById('pantalla').value = "Syntax Error";
             //console.log(this.memoria);
         }
     }  
@@ -96,15 +123,195 @@ class CalculadoraBasica{
 }
 class CalculadoraCientifica extends CalculadoraBasica{ 
     constructor(){
-        super();        
+        super();
+        this.arco = false;
+        this.hyperbolica = false;
+
     }
-    //x^2
-    botonPotenciaDeDos(){
+    //nuevas funciones de memoria
+    botonMemClear(){
+
+    }
+    botonMemRecall(){
+
+    }
+    botonMemSave(){
+
+    }
+
+    //IDK
+    botonUnidadDeAngulo(){
+
+    }
+    botonNotacionDiezElevado(){
+
+    }
+    botonRand(){
+
+    }
+    botonDms(){
+
+    }
+    botonDeg(){
+
+    }
+    //FUNCIONES TRIGONOMETRICAS
+    //sin sinh y sin^-1
+    botonSeno(){        
+        this.calcularResultado();        
+        this.seno();
+        this.display(); 
+    }
+    seno(){
+        if(!this.arco){            
+            if(!this.hyperbolica){
+                //seno sin                
+                this.pantalla = Math.sin(this.pantalla);                
+            }
+            else{                
+                //seno hyperbolico sinh
+                this.pantalla = Math.sinh(this.pantalla);   
+            }
+        }else{
+            if(!this.hyperbolica){
+                 //arcoseno sin^-1 asin
+                 this.pantalla = Math.asin(this.pantalla);   
+            }
+            else{
+                //arcoseno hyperbolico asinh
+                this.pantalla = Math.asinh(this.pantalla);   
+            }
+        }
+    }
+
+    botonCoseno(){        
+        this.calcularResultado();
+        this.coseno();
+        this.display(); 
+    }
+
+    coseno(){
+        if(!this.arco){            
+            if(!this.hyperbolica){
+                //seno sin                
+                this.pantalla = Math.cos(this.pantalla);                
+            }
+            else{                
+                //seno hyperbolico sinh
+                this.pantalla = Math.cosh(this.pantalla);   
+            }
+        }else{
+            if(!this.hyperbolica){
+                 //arcoseno sin^-1 asin
+                 this.pantalla = Math.acos(this.pantalla);   
+            }
+            else{
+                //arcoseno hyperbolico asinh
+                this.pantalla = Math.acosh(this.pantalla);   
+            }
+        }
+    }
+    botonTangente(){
+        this.calcularResultado();
+        this.tangente();
+        this.display(); 
+    }
+    tangente(){
+        if(!this.arco){            
+            if(!this.hyperbolica){
+                //seno sin                
+                this.pantalla = Math.tan(this.pantalla);                
+            }
+            else{                
+                //seno hyperbolico sinh
+                this.pantalla = Math.tanh(this.pantalla);   
+            }
+        }else{
+            if(!this.hyperbolica){
+                 //arcoseno sin^-1 asin
+                 this.pantalla = Math.atan(this.pantalla);   
+            }
+            else{
+                //arcoseno hyperbolico asinh
+                this.pantalla = Math.atanh(this.pantalla);   
+            }
+        }
+    }
+   
+    
+    botonSecante(){
+        this.calcularResultado();
+        this.seno();
+        this.pantalla = (1/parseFloat(this.pantalla)).toString();
+        this.display(); 
+    }
+    botonCosecante(){
+        this.calcularResultado();
+        this.coseno();
+        this.pantalla = (1/parseFloat(this.pantalla)).toString();
+        this.display(); 
+    }
+    botonCotangente(){
+        this.calcularResultado();
+        this.tangente();
+        this.pantalla = (1/parseFloat(this.pantalla)).toString();
+        this.display(); 
+    }
+    botonCambioArco(){//TODO falta hacer que cambien visualmente todos
+        if(this.arco){
+            this.arco = false;
+        }else{
+            this.arco = true;
+        }
+    }
+    botonCambioHyperbolic(){
+        if(this.hyperbolica){
+            this.hyperbolica = false;
+        }else{
+            this.hyperbolica = true;
+        }
+    }
+
+
+
+    botonCeil(){
+
+    }
+    botonFloor(){
+
+    }
+    botonCambioExponenciales(){
+
+    }
+    botonPi(){
+        
+    }
+    botonDms(){
+
+    }
+    botonQuitar(){
+
+    }
+    botonInverso(){
+
+    }
+    botonAbsolute(){
+
+    }
+    botonExp(){
+
+    }
+    botonMod(){
+
+    }
+
+    //x^2 y x^3
+    botonElevadoConstante(){//TODO Cambiar esto, fatla x^3 y hacer el eval bien
         this.pantalla = Math.pow(eval(this.consola,2)).toString();
         this.display();
     }
     //x^y
-    botonPotenciaDeX(){
+    botonExponencial(){
         //this.pantalla = Math.pow(eval(this.consola,2)).toString();
         //this.primerArgumento
         this.pantalla = "";
@@ -112,7 +319,7 @@ class CalculadoraCientifica extends CalculadoraBasica{
     }
     
     botonRaizCuadrada(){
-        this.pantalla = Math.sqrt(eval(this.pantalla)).toString();
+        this.pantalla = Math.sqrt(eval(this.pantalla)).toString();//TODO esto deberia hacer como en calcular resulado
         this.display();
     }
 
@@ -126,6 +333,18 @@ class CalculadoraCientifica extends CalculadoraBasica{
         this.auxMemory = false;
         this.pantalla+=")";
         this.display();   
+    }
+    botonFactorial(){
+
+    }
+    botonDiezElevado(){
+
+    }
+    botonLogaritmo(){
+
+    }
+    botonLogNeperiano(){
+
     }
 }
 var calculadora = new CalculadoraCientifica();
