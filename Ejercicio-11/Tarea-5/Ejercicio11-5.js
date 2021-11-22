bf2cc512b1551efbb16e5914aa13e98e310a0427
Manuel Lorenzo Vega <UO265009@uniovi.es>
2021-11-22 17:51:33 +0100

    intento de geolocalizacion con mapa

--- Ejercicio-11/Tarea-5/Ejercicio11-5.js
+++ Ejercicio-11/Tarea-5/Ejercicio11-5.js
@@ -8,21 +8,24 @@ class Map{
         this.apiKey = "pk.eyJ1IjoidW8yNjUwMDkiLCJhIjoiY2t3YW41MjU4Mm8wNDJ2cDZjamIzZmlyZiJ9.HI0OvDVrFK1OJeiT89a8Sg";
         this.container = "map";//la id del tag donde vamos a meter el mapa
         this.mapStyle = "mapbox://styles/mapbox/outdoors-v11";
-        this.center = [-3.7344397656591397, 40.43341788237872];//valor por defecto si el usaurio no da su posicion
-        this.zoom = 4;      
+        // this.center = [-3.7344397656591397, 40.43341788237872];
+        this.zoom = 4;
+        this.longitude = -3.7344397656591397;//valor por defecto si el usaurio no da su posicion
+        this.latitude = 40.43341788237872;
 
     }
-    mostrarMapaDinamico(){
-       
+    getPosicion(posicion){
+        this.longitude = posicion.coords.longitude;
+        this.latitude = posicion.coords.latitude;        
+    }
 
-       let longitude = posicion.coords.longitude;
-       let latitude = posicion.coords.latitude;
-       console.log(longitude);
+    mostrarMapaDinamico(){     
+       let center = [this.longitude,this.latitude];
        mapboxgl.accessToken = this.apiKey;
        const map = new mapboxgl.Map({
          container: this.container, // Specify the container ID
          style: this.mapStyle, // Specify which map style to use
-         center: this.center, // Specify the starting position [lng, lat]
+         center: center, // Specify the starting position [lng, lat]
          zoom: this.zoom // Specify the starting zoom
         });
        const marker1 = new mapboxgl.Marker()
