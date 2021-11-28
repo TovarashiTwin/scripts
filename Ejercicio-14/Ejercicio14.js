8af039f9d1d2656e8e818cf1c52f234039828d33
TovarashiTwin <tecmanuellor1516@gmail.com>
2021-11-28 19:08:08 +0100

    Ejercicio de tematica libre hecho!

---
+++ Ejercicio-14/Ejercicio14.js
@@ -0,0 +1,31 @@
+"use strict"
+class SVGVisualizer{    
+    constructor(){
+        this.svg = localStorage.getItem("svg");        
+    }
+    leerSVG(){
+        //LEEMOS EL FICHERO
+        let file = document.getElementById("inputArchivos").files[0];
+        //TODO meter que solo acepte svg
+        console.log(file.type);
+        let lector = new FileReader();
+        lector.readAsText(file);
+        var self = this;
+        lector.onload = function(e){
+            self.svg = lector.result;            
+            self.mostrarSVG();
+        }
+    }
+    mostrarSVG(){
+        if(this.svg != null){
+            localStorage.clear();
+            localStorage.setItem("svg",this.svg);
+            document.querySelector("main").innerHTML = this.svg;
+        }
+        
+    }
+    
+
+
+}
+var svgVisualizer = new SVGVisualizer();
