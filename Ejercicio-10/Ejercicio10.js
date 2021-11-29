a5e00bf9fd86b5e431ce6df2f0af4777b13fd9d4
TovarashiTwin <tecmanuellor1516@gmail.com>
2021-11-29 23:08:41 +0100

    Ejercicio api electricidad hecho

--- Ejercicio-10/Ejercicio10.js
+++ Ejercicio-10/Ejercicio10.js
@@ -1,26 +1,44 @@
 "use strict"
-class Electricidad{
-    // https://developer.nrel.gov/api/alt-fuel-stations/v1.json?fuel_type=E85,ELEC&state=CA&limit=2&api_key=uwHSTLf19qom3SEroWXvj1YhWS3Jnb7wRfOOzgo6
-    constructor(){
-        this.apikey = "uwHSTLf19qom3SEroWXvj1YhWS3Jnb7wRfOOzgo6";
-        this.format = "json";
-        this.datos = null;
+class Electricidad{    
+    constructor(){       
+        this.format = "json";        
     }
    
     cargarDatos(){
         var self = this;
         $.ajax({
             dataType: "json",
-            url: "https://developer.nrel.gov/api/utility_rates/v3.json?api_key=uwHSTLf19qom3SEroWXvj1YhWS3Jnb7wRfOOzgo6&lat=35.45&lon=-82.98",
+            url: "https://apidatos.ree.es/es/datos/mercados/componentes-precio?start_date=2019-01-01T00:00&end_date=2019-12-31T23:59&time_trunc=month",
             method: 'GET',
             success: function(data){
                 self.datos = data;
-                console.log(data)
+                console.log(data);                
+                self.visualizarDatos(data);                
             },
             error:function(){
                 alert("Ha ocurrido un error al acceder a la API");    
             }
         });
+    }
+    visualizarDatos(data){
+        //ESTO PASAR A jquery        
+        // console.log(data.data.attributes.title);//esto va a ser el h2
+        let tabla = "<table>"+"\n";
+        tabla += "<tr>"+"\n"+"<th>€/MWh</th>"+"\n"+"<th>fecha</th>";        
+        
+        // console.log(data.included[5].attributes);
+        let datosTabla = data.included[5].attributes.values;
+        for (const i in datosTabla) {            
+            let valor = Math.round(datosTabla[i].value*100)/100;
+            let time = datosTabla[i].datetime.split("T")[0];
+            // console.log(valor + "\t" + time);
+            tabla += "<tr>"+"\n";
+            tabla += "<td>"+valor+"</td>"+"\n";
+            tabla += "<td>"+time+"</td>"+"\n";
+            tabla += "</tr>"+"\n";
+        }
+        tabla += "</table>";
+        $("p:last").after(tabla);
     }
 
 }
